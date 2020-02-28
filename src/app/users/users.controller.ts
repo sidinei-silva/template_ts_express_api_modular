9eac171efd0a69d7653a7af751a86ccc6ac6dd06
Sidinei Silva <sidinei.silva02@gmail.com>
2020-02-28 13:39:26 -0300

    Import and usage user validator.

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -1,17 +1,27 @@
 import { Request, Response, Router } from 'express';
+import UserValidator from './users.validators';
 
 class UserController {
   private path: string;
 
+  private userValidator: UserValidator;
+
   constructor() {
     this.path = '/users';
+    this.userValidator = new UserValidator();
   }
 
   public initRoutes = (router: Router): void => {
     router.get(this.path, this.index);
+    router.post(this.path, this.userValidator.storeValidation, this.store);
   };
 
   private index = async (req: Request, res: Response): Promise<Response> => {
+    const user = 'Sidinei';
+    return res.json(user);
+  };
+
+  private store = async (req: Request, res: Response): Promise<Response> => {
     const user = 'Sidinei';
     return res.json(user);
   };
